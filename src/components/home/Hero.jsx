f66380bf05ccece32d775c76906bc2c93cbeaf60
pushkarydv <96358784+pushkarydv@users.noreply.github.com>
2022-03-20 11:06:13 +0530

    images

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -7,7 +7,6 @@ export default function Hero() {
         <div className="text-4xl mt-24 md:mt-0 lg:text-6xl xl:text-8xl font-bold text-slate-800 mb-8 lg:mb-12">
           Games to play on the go
         </div>
-
         <Link
           to={"/asd"}
           className="transition text-2xl lg:text-4xl bg-emerald-400 border-4 border-emerald-300 text-slate-50 py-2 px-6 rounded-full hover:text-slate-700 hover:bg-emerald-300 hover:border-emerald-400"
